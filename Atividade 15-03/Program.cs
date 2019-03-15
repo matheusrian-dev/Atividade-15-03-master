78a2986036f10f0645397f1f4bb219070d3b7816
matheusrian332 <matheusrian332@gmail.com>
2019-03-15 16:28:09 -0300

    clone feito

---
+++ Atividade 15-03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_15_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoje é sexta");
+            Console.WriteLine("Amanhã é sábado");
+            Console.WriteLine("Depois de amanhã é domingo");
+            Console.WriteLine("Depois de depois de amanhã é segunda");
+            Console.WriteLine("Depois de depois de depois de amanhã é terça");
+        }
+
+    }
+}
